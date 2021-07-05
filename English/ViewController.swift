44cbd202a5d43cef16e7266f04a1a84a6dda2cbe
hudihka <>
2021-07-05 13:43:34 +0700

    добавлен проект

---
+++ English/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  English
+//
+//  Created by Константин Ирошников on 05.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
