f09aff0f719c189526ab78854937ec5e9394f126
Константин Ирошников <VTB4050827@region.vtb.ru>
2021-07-30 14:15:39 +0700

    тест коммит

--- English/Models/Profile.swift
+++ English/Models/Profile.swift
@@ -9,45 +9,37 @@ import Foundation
 
 
 class Profile {
-    
-    var id = ""
+
     var lists: [List] = []
-    
-    init(id: String) {
-        self.id = id
-    }
-    
+
     init(json: JSON) {
-        
-        if let temp = json["id"] as? String {
-            self.id = temp
-        }
-        
+
         if let temp = json["lists"] as? [JSON] {
             let lists = temp.map { List(json: $0) }.sorted(by: { $0.dateUpdate > $1.dateUpdate })
-            
+
             self.lists = lists
         }
-        
+
     }
-    
+
 
     var json: JSON {
-        var json: [String : Any] = ["id" : self.id]
-        
+        var json: [String : Any] = [:]
+
         json["lists"]        = self.lists.map({$0.json})
-        
+
         return json
     }
-    
+
     var countFavorit: Int {
         let count = lists.reduce(0, {$0 + $1.countFavorit})
         return Int(count)
     }
-    
+
     var countWords: Int {
         let count = lists.reduce(0, {$0 + $1.count})
         return count
     }
-    
+
 }
+
